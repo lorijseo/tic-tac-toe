17da544adc7f6e6fce63928a4b586fad31240c39
lorijseo <lorijseo@gmail.com>
2022-04-13 18:14:14 -0700

    refactor function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,16 +1,4 @@
 import random
-game = True
-
-r1 = []
-r2 = []
-r3 = []
-for section in range(3):
-    r1.append("[ ]")
-    r2.append("[ ]")
-    r3.append("[ ]")
-board = [r1, r2, r3]
-
-check_repeats = []
 
 
 def show_board():
@@ -22,7 +10,7 @@ def show_board():
 
 def player_x_turn():
     show_board()
-    print("It's Player X's turn!")
+    print(f"It's {player_x}'s turn!")
     row_column = input("Write (row,column): ")
     if row_column not in check_repeats:
         check_repeats.append(row_column)
@@ -43,7 +31,7 @@ def player_x_turn():
 
 def player_o_turn():
     show_board()
-    print("It's Player O's turn!")
+    print(f"It's {player_o}'s turn!")
     row_column = input("Write (row,column): ")
     if row_column not in check_repeats:
         check_repeats.append(row_column)
@@ -62,14 +50,6 @@ def player_o_turn():
         player_o_turn()
 
 
-players = ["Player X", "Player O"]
-# player_1 = random.choice(players)
-# players.remove(player_1)
-# player_2 = players[0]
-player_x = players[0]
-player_o = players[1]
-
-
 def check_row_win(row_num):
     row_x_count = 0
     row_o_count = 0
@@ -80,12 +60,12 @@ def check_row_win(row_num):
             row_o_count += 1
     if row_x_count == 3:
         show_board()
-        print("Player X wins!")
-        return 888
+        print(f"{player_x} wins!")
+        return "win"
     elif row_o_count == 3:
         show_board()
-        print("Player O wins!")
-        return 888
+        print(f"{player_o}wins!")
+        return "win"
 
 
 def check_col_win(col_num):
@@ -98,74 +78,87 @@ def check_col_win(col_num):
             col_o_count += 1
     if col_x_count == 3:
         show_board()
-        print("Player X wins!")
-        return 888
+        print(f"{player_x} wins!")
+        return "win"
     elif col_o_count == 3:
         show_board()
-        print("Player O wins!")
-        return 888
+        print(f"{player_o} wins!")
+        return "win"
 
 
-def check_diagonal_1():
+def check_diagonal():
     x_count = 0
     o_count = 0
-    for index in range(3):
-        if board[index][index] == "[X]":
-            x_count += 1
-        elif board[index][index] == "[O]":
-            o_count += 1
-    if x_count == 3:
-        show_board()
-        print("Player X wins!")
-        return 888
-    elif o_count == 3:
-        show_board()
-        print("Player O wins!")
-        return 888
-
-
-def check_diagonal_2():
     if board[0][2] == "[X]" and board[1][1] == "[X]" and board[2][0] == "[X]":
         show_board()
-        print("Player X wins!")
-        return 888
+        print(f"{player_x} wins!")
+        return "win"
     elif board[0][2] == "[O]" and board[1][1] == "[O]" and board[2][0] == "[O]":
         show_board()
-        print("Player O wins!")
-        return 888
+        print(f"{player_o} wins!")
+        return "win"
+    else:
+        for index in range(3):
+            if board[index][index] == "[X]":
+                x_count += 1
+            elif board[index][index] == "[O]":
+                o_count += 1
+        if x_count == 3:
+            show_board()
+            print(f"{player_x} wins!")
+            return "win"
+        elif o_count == 3:
+            show_board()
+            print(f"{player_o} wins!")
+            return "win"
 
+
+game = True
+
+players = []
+check_repeats = []
+r1 = []
+r2 = []
+r3 = []
+for section in range(3):
+    r1.append("[ ]")
+    r2.append("[ ]")
+    r3.append("[ ]")
+board = [r1, r2, r3]
+
+print("Welcome to tic-tac-toe!\nLet's determine who goes first!")
+player_1 = input("Player1's name: ").upper()
+players.append(player_1)
+player_2 = input("Player2's name: ").upper()
+players.append(player_2)
+player_x = random.choice(players)
+players.remove(player_x)
+player_o = players[0]
+print(f"{player_x} will go first using X. {player_o} will go second using O")
 
 while game:
-    if check_row_win(r1) == 888 or check_row_win(r2) == 888 or check_row_win(r3) == 888 or check_col_win(1) == 888 or \
-            check_col_win(2) == 888 or check_col_win(3) == 888 or check_diagonal_1() == 888 or check_diagonal_2() == 888:
+    if check_row_win(r1) == "win" or check_row_win(r2) == "win" or check_row_win(r3) == "win" or \
+            check_col_win(1) == "win" or check_col_win(2) == "win" or check_col_win(3) == "win" or \
+            check_diagonal() == "win":
         print("Game over!")
         break
     else:
-        check_row_win(r1)
-        check_row_win(r2)
-        check_row_win(r3)
-        check_col_win(1)
-        check_col_win(2)
-        check_col_win(3)
-        check_diagonal_1()
-        check_diagonal_2()
+        for each_row in board:
+            check_row_win(each_row)
+        for each_col in range(1, 4):
+            check_col_win(each_col)
+        check_diagonal()
         player_x_turn()
 
-    if check_row_win(r1) == 888 or check_row_win(r2) == 888 or check_row_win(r3) == 888 or check_col_win(1) == 888 or \
-            check_col_win(2) == 888 or check_col_win(3) == 888 or check_diagonal_1() == 888 or check_diagonal_2() == 888:
+    if check_row_win(r1) == "win" or check_row_win(r2) == "win" or check_row_win(r3) == "win" or \
+            check_col_win(1) == "win" or check_col_win(2) == "win" or check_col_win(3) == "win" or \
+            check_diagonal() == "win":
         print("Game over!")
         break
     else:
-        check_row_win(r1)
-        check_row_win(r2)
-        check_row_win(r3)
-        check_col_win(1)
-        check_col_win(2)
-        check_col_win(3)
-        check_diagonal_1()
-        check_diagonal_2()
+        for each_row in board:
+            check_row_win(each_row)
+        for each_col in range(1, 4):
+            check_col_win(each_col)
+        check_diagonal()
         player_o_turn()
-
-# refactor functions
-# randomize player
-# choose player name
